338ad301d094fc75a6537fc485be56f7196dddf2
Luferan <159485852+Luferan@users.noreply.github.com>
2024-02-27 10:45:59 -0500

    Prueba 2
    Por fa que funcione

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -19,21 +19,69 @@ public class TV {
 		
 		TV.numTV++;
 	}
-	
 	public static int getNumTV() {
 		return TV.numTV;
 	}
-	
 	public static void setNumTV(int numTV) {
 		TV.numTV = numTV;
 	}
 	
-	public void turnON() {
+	public void turnOn() {
 		this.estado = true;
 	}
-	
 	public void turnOff() {
 		this.estado = false;
+	}
+	
+	
+	public void canalUp() {
+		this.setCanal(this.canal + 1);
+	}
+	public void canalDown() {
+		this.setCanal(this.canal - 1);
+	}
+	
+	public void volumenUp() {
+		this.setVolumen(this.volumen + 1);
+	}
+	public void volumenDown() {
+		this.setVolumen(this.volumen - 1);
+	}
+	
+	public Marca getMarca() {
+		return this.marca;
+	}
+	public boolean getEstado() {
+		return this.estado;
+	}
+	public int getCanal() {
+		return this.canal;
+	}
+	public int getVolumen() {
+		return this.volumen;
+	}
+	public int getPrecio() {
+		return this.precio;
+	}
+	public Control getControl() {
+		return this.control;
+	}
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	public void setCanal(int canal) {
+		if (this.estado && canal >= 1 && canal <= 120)
+			this.canal = canal;
+	}
+	public void setVolumen(int volumen) {
+		if(this.estado && this.volumen >= 0 && volumen <= 7)
+			this.volumen = volumen;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public void setControl(Control control) {
+		this.control = control;
 	}
 	
 }
